89fee5689e026f97a60a81c36533e8696aa77028
perfogic <longdieu12x@gmail.com>
2025-03-17 21:30:37 +0700

    chore: add set liquidity for position

--- src/contracts/core/PTonMinter.ts
+++ src/contracts/core/PTonMinter.ts
@@ -1,58 +1,73 @@
-import { Address, beginCell, Cell, contractAddress, ContractProvider, Sender, SendMode, toNano } from '@ton/core';
+import {
+  Address,
+  beginCell,
+  Cell,
+  contractAddress,
+  ContractProvider,
+  Sender,
+  SendMode,
+  toNano,
+} from '@ton/core';
 import JettonMinterWrapper from './JettonMinter';
 import { PTON_MASTER_BOC } from '../../constants';
 
 export namespace PTonMinterWrapper {
+  export type MinterConfig = {
+    id?: number;
+    walletCode: Cell;
+    content: Cell;
+  };
 
-    export type MinterConfig = {
-        id?: number,
-        walletCode: Cell,
-        content: Cell,
-    };
+  export function minterConfigToCell(config: MinterConfig): Cell {
+    return beginCell()
+      .storeUint(config.id || 0, 32)
+      .storeRef(config.walletCode)
+      .storeRef(config.content)
+      .endCell();
+  }
 
-    export function minterConfigToCell(config: MinterConfig): Cell {
-        return beginCell()
-            .storeUint(config.id || 0, 32)
-            .storeRef(config.walletCode)
-            .storeRef(config.content)
-            .endCell();
+  export const proxyOpCodesV2 = {
+    ...JettonMinterWrapper.JettonMinterOpCodes,
+    deployWallet: 0x4f5f4313,
+  } as const;
+
+  export class PTonMinterV2 extends JettonMinterWrapper.JettonMinter {
+    constructor(
+      readonly address: Address,
+      readonly init?: { code: Cell; data: Cell },
+    ) {
+      super(address, init);
     }
 
-    export const proxyOpCodesV2 = {
-        ...JettonMinterWrapper.JettonMinterOpCodes,
-        deployWallet: 0x4f5f4313
-    } as const;
-
-
-    export class PTonMinterV2 extends JettonMinterWrapper.JettonMinter {
-        constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell; }) {
-            super(address, init)
-        }
-
-        static createPtonMinterFromConfig(config: MinterConfig,  workchain = 0) {
-            const code = Cell.fromBoc(Buffer.from(PTON_MASTER_BOC, 'hex'))[0];
-            const data = minterConfigToCell(config);
-            const init = { code, data };
-            return new PTonMinterV2(contractAddress(workchain, init), init);
-        }
-
-        async sendDeployWallet(provider: ContractProvider, via: Sender, opts: {
-            value?: bigint,
-            ownerAddress: Address,
-            excessesAddress?: Address
-        }, value?: bigint) {
-            return await provider.internal(via, {
-                value: value ?? toNano("1"),
-                sendMode: SendMode.PAY_GAS_SEPARATELY,
-                body: beginCell()
-                    .storeUint(proxyOpCodesV2.deployWallet, 32)
-                    .storeUint(0, 64)
-                    .storeAddress(opts.ownerAddress)
-                    .storeAddress(opts.excessesAddress || via.address)
-                    .endCell()
-            });
-        }
+    static createPtonMinterFromConfig(config: MinterConfig, workchain = 0) {
+      const code = Cell.fromBoc(Buffer.from(PTON_MASTER_BOC, 'hex'))[0];
+      const data = minterConfigToCell(config);
+      const init = { code, data };
+      return new PTonMinterV2(contractAddress(workchain, init), init);
     }
+
+    async sendDeployWallet(
+      provider: ContractProvider,
+      via: Sender,
+      opts: {
+        value?: bigint;
+        ownerAddress: Address;
+        excessesAddress?: Address;
+      },
+      value?: bigint,
+    ) {
+      return await provider.internal(via, {
+        value: value ?? toNano('1'),
+        sendMode: SendMode.PAY_GAS_SEPARATELY,
+        body: beginCell()
+          .storeUint(proxyOpCodesV2.deployWallet, 32)
+          .storeUint(0, 64)
+          .storeAddress(opts.ownerAddress)
+          .storeAddress(opts.excessesAddress || via.address)
+          .endCell(),
+      });
+    }
+  }
 }
 
 export default PTonMinterWrapper;
--- src/contracts/core/PTonWallet.ts
+++ src/contracts/core/PTonWallet.ts
@@ -1,99 +1,116 @@
-import { Address, beginCell, Cell, ContractProvider, ExternalAddress, Sender, SendMode, Slice } from '@ton/core';
+import {
+  Address,
+  beginCell,
+  Cell,
+  ContractProvider,
+  ExternalAddress,
+  Sender,
+  SendMode,
+  Slice,
+} from '@ton/core';
 import JettonWalletWrapper from './JettonWallet';
 import { ValueOps } from '..';
 import { OpJettonTransferMint, OpJettonTransferSwap } from '../../tlbs/jetton';
 
 export namespace PTonWalletWrapper {
-    export type WalletConfig = {
-        balance: bigint,
-        ownerAddress: Address,
-        minterAddress: Address,
-    };
+  export type WalletConfig = {
+    balance: bigint;
+    ownerAddress: Address;
+    minterAddress: Address;
+  };
 
-    export function walletConfigToCell(config: WalletConfig): Cell {
-        return beginCell()
-            .storeCoins(config.balance)
-            .storeAddress(config.ownerAddress)
-            .storeAddress(config.minterAddress)
-            .endCell();
+  export function walletConfigToCell(config: WalletConfig): Cell {
+    return beginCell()
+      .storeCoins(config.balance)
+      .storeAddress(config.ownerAddress)
+      .storeAddress(config.minterAddress)
+      .endCell();
+  }
+
+  export const proxyWalletOpcodesV2 = {
+    ...JettonWalletWrapper.JettonOpCodes,
+    resetGas: 0x29d22935,
+    tonTransfer: 0x01f3835d,
+  } as const;
+
+  export class PTonWalletV2 extends JettonWalletWrapper.JettonWallet {
+    constructor(
+      readonly address: Address,
+      readonly init?: { code: Cell; data: Cell },
+    ) {
+      super(address, init);
     }
 
-    export const proxyWalletOpcodesV2 = {
-        ...JettonWalletWrapper.JettonOpCodes,
-        resetGas: 0x29d22935,
-        tonTransfer: 0x01f3835d
-    } as const;
-
-    export class PTonWalletV2 extends JettonWalletWrapper.JettonWallet {
-        constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell; }) {
-            super( address, init)
-        }
-
-        static createFromConfig(config: JettonWalletWrapper.JettonWalletConfig, workchain = 0) {
-            return JettonWalletWrapper.JettonWallet.createFromConfig(config, workchain)
-        }
-
-        static createFromAddress(address: Address) {
-            return new PTonWalletV2(address);
-        }
-
-        async sendTransferMint(provider: ContractProvider, via: Sender, data: OpJettonTransferMint, opts: ValueOps): Promise<void> {
-            throw new Error("Not implemented");
-        }
-
-        async sendTransferSwap(provider: ContractProvider, via: Sender, data: OpJettonTransferSwap, opts: ValueOps): Promise<void> {
-            throw new Error("Not implemented");
-        }
-
-        async sendTonTransfer(provider: ContractProvider, via: Sender, opts: {
-            tonAmount: bigint,
-            refundAddress: Address | ExternalAddress | null
-            fwdPayload: Cell | Slice,
-            gas: bigint,
-            noPayloadOverride?: boolean // only used to test refund
-        }, value?: bigint) {
-            if (!opts.gas) throw new Error("gas is 0")
-
-            let msg_builder = beginCell()
-                .storeUint(proxyWalletOpcodesV2.tonTransfer, 32)
-                .storeUint(0, 64)
-                .storeCoins(opts.tonAmount)
-                .storeAddress(opts.refundAddress)
-
-            let msg: Cell;
-            if (opts.noPayloadOverride) {
-                msg = msg_builder.endCell();
-            } else {
-                if (opts.fwdPayload instanceof Cell) {
-                    msg = msg_builder
-                        .storeUint(1, 1)
-                        .storeRef(opts.fwdPayload)
-                        .endCell();
-                } else {
-                    msg = msg_builder
-                        .storeUint(0, 1)
-                        .storeSlice(opts.fwdPayload)
-                        .endCell();
-                }
-            }
-
-            await provider.internal(via, {
-                value: value ?? (opts.tonAmount + opts.gas),
-                sendMode: SendMode.PAY_GAS_SEPARATELY,
-                body: msg,
-            });
-        }
-
-        async sendResetGas(provider: ContractProvider, via: Sender, value: bigint) {
-            await provider.internal(via, {
-                value: value,
-                sendMode: SendMode.PAY_GAS_SEPARATELY,
-                body: beginCell()
-                    .storeUint(proxyWalletOpcodesV2.resetGas, 32)
-                    .storeUint(0, 64)
-                    .endCell(),
-            });
-        }
-
+    static createFromConfig(config: JettonWalletWrapper.JettonWalletConfig, workchain = 0) {
+      return JettonWalletWrapper.JettonWallet.createFromConfig(config, workchain);
     }
+
+    static createFromAddress(address: Address) {
+      return new PTonWalletV2(address);
+    }
+
+    async sendTransferMint(
+      provider: ContractProvider,
+      via: Sender,
+      data: OpJettonTransferMint,
+      opts: ValueOps,
+    ): Promise<void> {
+      throw new Error('Not implemented');
+    }
+
+    async sendTransferSwap(
+      provider: ContractProvider,
+      via: Sender,
+      data: OpJettonTransferSwap,
+      opts: ValueOps,
+    ): Promise<void> {
+      throw new Error('Not implemented');
+    }
+
+    async sendTonTransfer(
+      provider: ContractProvider,
+      via: Sender,
+      opts: {
+        tonAmount: bigint;
+        refundAddress: Address | ExternalAddress | null;
+        fwdPayload: Cell | Slice;
+        gas: bigint;
+        noPayloadOverride?: boolean; // only used to test refund
+      },
+      value?: bigint,
+    ) {
+      if (!opts.gas) throw new Error('gas is 0');
+
+      let msg_builder = beginCell()
+        .storeUint(proxyWalletOpcodesV2.tonTransfer, 32)
+        .storeUint(0, 64)
+        .storeCoins(opts.tonAmount)
+        .storeAddress(opts.refundAddress);
+
+      let msg: Cell;
+      if (opts.noPayloadOverride) {
+        msg = msg_builder.endCell();
+      } else {
+        if (opts.fwdPayload instanceof Cell) {
+          msg = msg_builder.storeUint(1, 1).storeRef(opts.fwdPayload).endCell();
+        } else {
+          msg = msg_builder.storeUint(0, 1).storeSlice(opts.fwdPayload).endCell();
+        }
+      }
+
+      await provider.internal(via, {
+        value: value ?? opts.tonAmount + opts.gas,
+        sendMode: SendMode.PAY_GAS_SEPARATELY,
+        body: msg,
+      });
+    }
+
+    async sendResetGas(provider: ContractProvider, via: Sender, value: bigint) {
+      await provider.internal(via, {
+        value: value,
+        sendMode: SendMode.PAY_GAS_SEPARATELY,
+        body: beginCell().storeUint(proxyWalletOpcodesV2.resetGas, 32).storeUint(0, 64).endCell(),
+      });
+    }
+  }
 }
