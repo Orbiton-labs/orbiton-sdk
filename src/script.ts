686503b53e999cb2df33868b3ee14569a3e16d9b
perfogic <longdieu12x@gmail.com>
2025-03-18 23:33:40 +0700

    chore: update script

--- src/script.ts
+++ src/script.ts
@@ -10,9 +10,11 @@ import { mnemonicToWalletKey } from '@ton/crypto';
 import { setTimeout } from 'timers/promises';
 import { PositionMath, TickMath } from './utils';
 import { maxLiquidityForAmounts } from './utils/maxLiquidityForAmounts';
-import { MaxUint256 } from './constants';
+import { Chain, MaxUint256, MINIMUM_LIQUIDITY } from './constants';
 
-const poolAddr = 'EQCtpowhg8efNm364J51zDiKNT_CNnApUU-bor5Jpd7HzhR3';
+const poolAddr = 'EQBDjvLUuBHTFabA5U5YhoXRjTBeh9HKuMNy2us-7j2GBN1Z';
+export const MIN_SQRT_RATIO = 4295128739n;
+export const MAX_SQRT_RATIO = 1461446703485210103287273052203988822378723970342n;
 
 const main = async () => {
   const tc = new TonClient({
@@ -20,126 +22,44 @@ const main = async () => {
       network: 'testnet',
     }),
   });
+  const tonApiClient = new TonApiClient({
+    baseUrl: `https://testnet.tonapi.io`,
+    apiKey: 'AGSNOVUCGDJF32AAAAANN2LHR6HNAGLSAAHD4X3MNJGOJNJ45MAK4JNTWOT3V5RHGNJC5QY',
+  });
 
   const pool = tc.open(PoolWrapper.Pool.createFromAddress(Address.parse(poolAddr)));
   const poolInfo = await pool.getPoolInfo();
-
-  // const { publicKey, secretKey } = await mnemonicToWalletKey(''.split(' '));
-  // const wallet = WalletContractV4.create({
-  //   publicKey,
-  //   workchain: 0,
-  // });
-  // const walletContract = tc.open(wallet);
-  // const sender = walletContract.sender(secretKey);
-
-  // const ta = new TonApiClient({
-  //   baseUrl: `https://testnet.tonapi.io`,
-  //   apiKey: '',
-  // });
+  const jettons = await pool.getJettonsWallet();
   const usdc = new Jetton('EQCF8jfV05w00abPcvsW64XNanQ9vateIhCLSkNAQ7Qfo-WW', 9, 'USDC');
   const orb = new Jetton('EQCqaCb9S8wqYjPT1d18Z0f-HemRnEDm4heFyNfPKMESAIjQ', 9, 'Orbiton Swap');
-  const usdcUser = new Jetton('EQCF8jfV05w00abPcvsW64XNanQ9vateIhCLSkNAQ7Qfo-WW', 9, 'USDC');
-  const orbUser = new Jetton('EQCqaCb9S8wqYjPT1d18Z0f-HemRnEDm4heFyNfPKMESAIjQ', 9, 'Orbiton Swap');
   await Promise.all([
-    usdc.setWalletAddress(tc, Address.parse('EQCeuRLbIAm__PPiU-Ej-D6iR_4K1wAdF_ABttWUw086IzZu')),
-    orb.setWalletAddress(tc, Address.parse('EQCeuRLbIAm__PPiU-Ej-D6iR_4K1wAdF_ABttWUw086IzZu')),
-    usdcUser.setWalletAddress(
-      tc,
-      Address.parse('0QBkxNmedeIS12e9bD0PO6nPaRMpU3dnijh90OH5Dtjqpbjb'),
-    ),
-    orbUser.setWalletAddress(tc, Address.parse('0QBkxNmedeIS12e9bD0PO6nPaRMpU3dnijh90OH5Dtjqpbjb')),
+    usdc.setWalletAddress(tc, Address.parse('EQCUjc3HQ5T56UY8uYm_b0FMY68uOK8OT67wVwmUXdnLMAq9')),
+    orb.setWalletAddress(tc, Address.parse('EQCUjc3HQ5T56UY8uYm_b0FMY68uOK8OT67wVwmUXdnLMAq9')),
   ]);
 
-  // const { messages, result } = await PoolMessageBuilder.createEmulatedMintMessage(
-  //   ta,
-  //   WalletVersion.V4R2,
-  //   Address.parse('0QBkxNmedeIS12e9bD0PO6nPaRMpU3dnijh90OH5Dtjqpbjb'),
-  //   Address.parse('EQCeuRLbIAm__PPiU-Ej-D6iR_4K1wAdF_ABttWUw086IzZu'),
-  //   usdc, // router 0
-  //   orb, // router 1
-  //   JettonAmount.fromRawAmount(usdcUser, 1000000000000n), // user 0
-  //   JettonAmount.fromRawAmount(orbUser, 1000000000000n), // user 1
-  //   60, // tick spacing
-  //   3000, // fee
-  //   -240000n, // tick lower
-  //   120000n, // tick upper
-  //   316475945059n,
-  //   Address.parse('0QBkxNmedeIS12e9bD0PO6nPaRMpU3dnijh90OH5Dtjqpbjb'),
-  // );
-
-  // for (const message of messages) {
-  //   await sender.send(message);
-  //   await setTimeout(1000);
-  // }
-
-  // const cell = Cell.fromBoc(
-  //   Buffer.from(
-  //     'b5ee9c720101030100da000285800c989b33cebc425aecf7ad87a1e77539ed22652a6eecf1470fba1c3f21db1d54b00193136679d7884b5d9ef5b0f43ceea73da44ca54ddd9e28e1f74387e43b63aa96010200ce0000000000000000000000000000000000000000000000000000001735de120800000000000000000000000000000000000000000000000000000002540be400000000000000000050f6efdcc3dcdce8de36503c00000000000000000000d65d599a4da2ffa60f005000000000000000000000000000000000000000000000000000000000000000000000000000000000',
-  //     'hex',
-  //   ),
-  // )[0];
-  // const fistRef = cell.asSlice().loadRef().beginParse();
-  // const amount0 = fistRef.loadInt(256);
-  // const amount1 = fistRef.loadInt(256);
-  // const sqrtPriceX96 = fistRef.loadUintBig(160);
-  // console.log({ amount0, amount1, sqrtPriceX96 });
-
-  const sqrtRatioAX96 = TickMath.getSqrtRatioAtTick(-24060);
-  const sqrtRatioBX96 = TickMath.getSqrtRatioAtTick(-22080);
-  console.log({ sqrtRatioAX96, sqrtRatioBX96 });
-  console.log(poolInfo.tick);
-  console.log({
-    sqrtRatioCurrentX96: 25060680620470481165591749524n,
-    sqrtRatioAX96,
-    sqrtRatioBX96,
-    amount0: 4999999999n + 1n, // 5 usdc
-    amount1: MaxUint256,
-    useFullPrecision: false,
-  });
-  // ạksdhlaksdjalsksd
-  const liquidity = maxLiquidityForAmounts(
-    25060680620470481165591749524n,
-    sqrtRatioAX96,
-    sqrtRatioBX96,
-    4999999999n + 1n,
-    MaxUint256,
-    true,
+  const zeroForOne = jettons[0].toString() === usdc.address.toString();
+  const emulatedResult = await PoolMessageBuilder.createEmulatedExactInSwapMessage(
+    tonApiClient,
+    WalletVersion.V4R2,
+    Address.parse('0QCmjcMi-GJ32FQe27fmL5n9a1Ab091gahJWaxu0uHasMeGt'),
+    JettonAmount.fromRawAmount(usdc, 10000000n),
+    orb,
+    Number(poolInfo.tickSpacing),
+    Number(poolInfo.fee),
+    zeroForOne ? MIN_SQRT_RATIO + 1n : MAX_SQRT_RATIO - 1n,
+    Address.parse('0QCmjcMi-GJ32FQe27fmL5n9a1Ab091gahJWaxu0uHasMeGt'),
+    zeroForOne ? -1 : 0,
+    false,
+    0,
+    Chain.Testnet,
+    {
+      ROUTER: 'EQCtpowhg8efNm364J51zDiKNT_CNnApUU-bor5Jpd7HzhR3',
+      PTON_ROUTER_WALLET: '',
+    },
   );
-  console.log('V1:', { liquidity });
-  const position = Position.fromAmount0({
-    pool: new Pool(
-      usdc,
-      orb,
-      FeeAmount.MEDIUM,
-      25060680620470481165591749524n,
-      235765193386072n,
-      Number(-23022),
-      Number(60),
-    ),
-    amount0: 4999999999n + 1n,
-    tickLower: -24060,
-    tickUpper: -22080,
-    useFullPrecision: true,
-  });
-  const { amount0, amount1 } = position;
-  console.log({
-    sqrtRatioCurrentX96: 25060680620470481165591749524n,
-    sqrtRatioAX96,
-    sqrtRatioBX96,
-    amount0: 4999999999n + 1n,
-    amount1: amount1.quotient,
-    useFullPrecision: false,
-  });
-  // ạksdhlaksdjalsksd
-  const newLiquidity = maxLiquidityForAmounts(
-    25060680620470481165591749524n,
-    sqrtRatioAX96,
-    sqrtRatioBX96,
-    4999999999n + 1n,
-    amount1.quotient,
-    true,
-  );
-  console.log('V2:', { newLiquidity });
+
+  const transaction = em;
+  console.dir(emulatedResult.result.children, { depth: null });
 };
 
 main();
